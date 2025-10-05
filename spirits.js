f504d5434074955c1a78b08a2fe7a014f2c4d284
NP360VMO <np360.vmo@gmail.com>
2025-10-05 10:32:26 +0800

    Add files via upload

--- spirits.js
+++ spirits.js
@@ -4,59 +4,59 @@
   window.SPIRITS = [
   {
     "id": "spirit1",
-    "name": "纜雲仔（標準形態）",
+    "name": "纜雲仔（標準形態）Cable Cloud (Standardmon)",
     "image": "images/cablecar-spirit.png",
     "page": "spirit1.html",
     "type": "探索系",
-    "prompt": "探索昂坪纜車站旁的圓形廣場以解鎖纜雲仔（標準形態）！"
+    "prompt": "探索昂坪纜車站旁的圓形廣場以解鎖纜雲仔（標準形態）！Explore the Stupa next to the Ngong Ping Cable Car Station to unlock Cable Cloud (Standardmon) to unlock this NPmon!"
   },
   {
     "id": "spirit2",
-    "name": "纜雲仔（水晶形態）",
+    "name": "纜雲仔（水晶形態）Cable Cloud (Crsytalmon)",
     "image": "images/crystal-spirit.png",
     "page": "spirit2.html",
     "type": "探索系",
-    "prompt": "探索與佛同行精品店附近以解鎖纜雲仔（水晶形態）！"
+    "prompt": "探索與佛同行精品店附近以解鎖纜雲仔（水晶形態）！Explore the area around Walking with Buddha Shop to unlock Cable Cloud (Crystal) to unlock this NPmon!"
   },
   {
     "id": "spirit3",
-    "name": "纜雲仔（全景形態）",
+    "name": "纜雲仔（全景形態）Cable Cloud (Crystal+mon)",
     "image": "images/panorama-spirit.png",
     "page": "spirit3.html",
     "type": "探索系",
-    "prompt": "探索#HONGKONG精品店以解鎖纜雲仔（全景形態）！"
+    "prompt": "探索#HONGKONG精品店以解鎖纜雲仔（全景形態）！Explore the area around #HONGKONG Shop to unlock Cable Cloud (Crystal+ Mon) to unlock this NPmon!"
   },
   {
     "id": "spirit_game1",
-    "name": "黃哞哞",
+    "name": "黃哞哞 Golden Moon Mon",
     "image": "images/game-spirit1.png",
     "page": "spirit_game1.html",
     "type": "🎮 挑戰系",
-    "prompt": "探索昂坪市集拱門完成黃哞哞挑戰遊戲以解鎖此精靈！"
+    "prompt": "探索昂坪市集拱門完成黃哞哞挑戰遊戲以解鎖此精靈！Explore the Ngong Ping Village Archway and complete the Golden Moo Moo Challenge to unlock this NPmon!"
   },
   {
     "id": "spirit_game2",
-    "name": "小黃彈彈",
+    "name": "小黃彈彈 Little Yellow BB",
     "image": "images/game-spirit2.png",
     "page": "spirit_game2.html",
     "type": "🎮 挑戰系",
-    "prompt": "探索市集菩提樹挑戰小黃彈彈遊戲以解鎖此精靈！"
+    "prompt": "探索市集菩提樹挑戰小黃彈彈遊戲以解鎖此精靈！Explore the Bodhi tree in the market and challenge the Little Yellow BB game to unlock this NPmon!"
   },
   {
     "id": "spirit_game3",
-    "name": "飲茶仔",
+    "name": "飲茶仔 Dim Sum Boy",
     "image": "images/game-spirit3.png",
     "page": "spirit_game3.html",
     "type": "🎮 挑戰系",
-    "prompt": "探索昂坪纜車站旁壁畫牆挑戰飲茶仔以解鎖此精靈！"
+    "prompt": "探索昂坪纜車站旁壁畫牆挑戰飲茶仔以解鎖此精靈！Explore the murals near Ngong Ping Cable Car Station and challenge the Dim Sum Boy to unlock this NPmon!"
   },
   {
     "id": "spirit_game4",
-    "name": "珍奶雞蛋仔",
+    "name": "珍奶雞蛋仔 Boba Egg Waffle",
     "image": "images/game-spirit4.png",
     "page": "spirit_game4.html",
     "type": "🎮 挑戰系",
-    "prompt": "遊玩纜車探知館尋找珍奶雞蛋仔挑戰任務以解鎖此精靈！"
+    "prompt": "遊玩纜車探知館尋找珍奶雞蛋仔挑戰任務以解鎖此精靈！Unlock this NPmon by completing the Boba Egg Waffle Challenge at the Cable Car Discovery Center!"
   }
 ];
 })();
